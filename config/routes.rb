7d26ab64608b4f7d4fe60faade253cab2cb7ea92
Victor Sanchez <vsanantone@gmail.com>
2023-10-14 23:06:03 -0500

    feat:add routes  citizens index, citizen show and country citizens index

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   get '/countries', to: "countries#index"
   get '/countries/:id', to: "countries#show"
+  get '/citizens', to: "citizens#index"
+  get '/citizens/:id', to: "citizens#show"
+  get '/countries/:id/citizens', to: "country_citizens#index" #????
 end
