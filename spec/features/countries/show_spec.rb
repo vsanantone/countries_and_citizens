0a77b690c0d65ca267e1a59a042f97a800382a6c
Victor Sanchez <vsanantone@gmail.com>
2023-10-17 19:08:08 -0500

    test: test for presence of Kountry Citizen Index link

--- spec/features/countries/show_spec.rb
+++ spec/features/countries/show_spec.rb
@@ -1,35 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "describing country features" do
-  describe "Country Index" do
+RSpec.describe "Country Show" do
     it "shows the country with the id including the country's attributes" do
-      # As a visitor
-      # When I visit '/countries/:id'
-      # Then I see the countries with that id including the countries' attributes
-      # (data from each column that is on the countries table)
       #set up
       israel = Country.create!(name: "Israel", democratic: true, year_founded: 1948)
       usa = Country.create!(name: "USA", democratic: true, year_founded: 1776)
       phillipines = Country.create!(name: "Phillipines", democratic: true, year_founded: 1800)
-    
       #action
       visit "/countries/#{israel.id}"
-
       #test
-      # save_and_open_page
       expect(page).to have_content(israel.name)
       expect(page).to have_content(israel.democratic)
       expect(page).to have_content(israel.year_founded)
-    end
   end
 
-  describe "Country citizen Count" do
+  describe "Country Citizen Count" do
     it "I see a count of the number of citizen's associated with this Country" do
-      # Country Citizen Count
-      # As a visitor
-      # When I visit a Country's show page
-      #I see a count of the number of citizen's associated with this Country
-      
       israel = Country.create!(name: "Israel", democratic: true, year_founded: 1948)
       usa = Country.create!(name: "USA", democratic: true, year_founded: 1776)
       phillipines = Country.create!(name: "Phillipines", democratic: true, year_founded: 1800)
@@ -49,4 +35,30 @@ RSpec.describe "describing country features" do
       expect(page).to have_content("How many citizens? 2")
     end
   end
+
+  describe "Country Citizen Index Link" do
+    it "displays a link that redirects to the Country's Citizens' index page" do
+      israel = Country.create!(name: "Israel", democratic: true, year_founded: 1948)
+
+      visit "/countries/#{israel.id}"
+      expect(page).to have_link("Israel's Citizen Index", href: "/countries/#{israel.id}/citizens")
+    end
+  end
+
+  # describe "Country Citizen Index Link" do
+  #   it "displays a link that redirects to the Country's Citizens' index page" do
+  #     israel = Country.create!(name: "Israel", democratic: true, year_founded: 1948)
+
+  #     visit "/countries/#{israel.id}"
+  #     expect(page).to have_link("Israel's Citizen Index") 
+  #   end
+
+  #   it "redirects user to the Country Citizen Index page" do
+  #     israel = Country.create!(name: "Israel", democratic: true, year_founded: 1948)
+
+  #     visit "/countries/#{israel.id}"
+  #     click_link("Israel's Citizen Index") 
+  #     expect(page).to have_current_path("/countries/#{israel.id}/citizens")
+  #   end
+  # end
 end
